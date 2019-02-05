aac615ef58df199c6fbbc1f92e513775352c18cd
yasu320 <yasu320.musashi@gmail.com>
2019-02-06 04:48:45 +0900

    complete signup and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,6 @@ class User < ApplicationRecord
     false
   end
 
-  def email_changed?
-    false
-  end
-
   # use this instead of email_changed? for Rails = 5.1.x
   def will_save_change_to_email?
     false
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#home'
   get "/rule" => "static_pages#rule"
+  resources :users, only: [:show]
 
 end
